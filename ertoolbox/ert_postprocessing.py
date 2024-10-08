3cc3a6b9c77bcdb238931709a7083c4d0ead7ae7
LeentvaarDeltares <eline.leentvaar@deltares.nl>
2024-10-08 09:35:05 +0200

    add sal to conductivity function to postprocessing

--- ertoolbox/ert_postprocessing.py
+++ ertoolbox/ert_postprocessing.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 # not yet implemented in parser object!!!
 
 
-def conductivity_to_salinity(conductivity, temp):
+def conductivity_to_salinity(conductivity, temperature):
     """
     Convert conductivity and corresponding temperature value to salinity
     1-value only functioin
@@ -26,7 +26,32 @@ def conductivity_to_salinity(conductivity, temp):
 
     """
 
-    mt = 1 / (np.power(0.008018 * temp + 1.0609, 2) - 0.5911)
+    mt = 1 / (np.power(0.008018 * temperature + 1.0609, 2) - 0.5911)
     salinity = np.power(conductivity * mt / 2.134, 1 / 0.92)
 
     return salinity
+
+
+def salinity_to_conductivity(salinity, temperature):
+    """
+    Convert salinity and corresponding temperature value to conductivity
+    1-value only functioin
+
+    Parameters
+    ----------
+    salinity : float
+        Salinity value in [ ]
+
+    temp : float
+        Temperature [Celcius]
+
+    Returns
+    -------
+    conductivity : float
+        Conductivity [mS/m]
+
+    """
+    mt = 1 / (np.power(0.008018 * temperature + 1.0609, 2) - 0.5911)
+    conductivity = (salinity**0.92 * 2.134) / mt
+
+    return conductivity
