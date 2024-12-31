4638d0628c55b13dedd97d1ac2169cf3de00bcb2
LeentvaarDeltares <eline.leentvaar@deltares.nl>
2024-12-31 12:25:07 +0100

    import remove measurements module

--- ertoolbox/__init__.py
+++ ertoolbox/__init__.py
@@ -1,4 +1,5 @@
-#import most used function from ertoolbox
+# import most used function from ertoolbox
 from ertoolbox.ert_inversion import inversion
 from ertoolbox.ert_parsers import MPT
 from ertoolbox.ert_preprocessing import replace_electrode_positions
+from ertoolbox.ert_preprocessing import remove_measurements
